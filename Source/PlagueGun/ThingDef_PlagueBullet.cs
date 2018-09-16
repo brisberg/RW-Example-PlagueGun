095feaaa694cca0a0e1c8c6f0e30905f00b8827a
Brandon Risberg <brandon.risberg@gmail.com>
2018-09-15 17:55:03 -0700

    Initial commit from tutorial

---
+++ Source/PlagueGun/ThingDef_PlagueBullet.cs
@@ -0,0 +1,20 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Plague
+{
+  public class ThingDef_PlagueBullet : ThingDef
+  {
+    public float AddHediffChance = 0.05f; //The default chance of adding a hediff.
+    public HediffDef HediffToAdd;
+
+    public override void ResolveReferences()
+    {
+      base.ResolveReferences();
+      LongEventHandler.ExecuteWhenFinished(delegate
+      {
+        this.HediffToAdd = HediffDefOf.Plague;
+      });
+    }
+  }
+}
